52eb261b0ccdb0a7b8bba9f798cf4c71dedb69fb
Derik Strattan <derik.strattan@gmail.com>
2013-10-10 15:24:52 -0700

    All the twitter-bootstrap failure + fixes.

---
+++ README.md
@@ -0,0 +1,3 @@
+Epic Twitter Bootstrap Fail && other useful tips
+
+This README intends to be a resource for debugging your twitter-bootstrap Railscast #328.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = 'e749da1cae3cfc7e58306db9df2b4cd19fcc9da56230aab07224827f41ec551967b5227699fab5f32fe41f2903095cf9404a4a0048a19794ee2071f18f56fe9d'
